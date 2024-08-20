ac1d321c318b6c85e18cda892011f7a6c07976b6
João Apostulo Neto <japostulo@gmail.com>
2024-08-20 19:44:06 -0300

    Adiciona cores do tenant em cache no storage.
    - Adicionado a recuperação do tenant que agora é armazenado no local storage
    com uma função de recuperação que faz o decode do string json nested.
    - Adicionado função para setar cor de thema conforme o que está no local
    storage e o que foi recebido via api.

--- src/modules/tenant/store.ts
+++ src/modules/tenant/store.ts
@@ -5,7 +5,7 @@ import { useTheme } from 'vuetify';
 
 const useTenantStore = defineStore('tenant', () => {
 
-    const tenant = ref<Tenant>(JSON.parse(localStorage.getItem('tenant')!) ?? {
+    const tenant = ref<Tenant>(getFromStorage('tenant') ?? {
         id: 0,
         name: '',
         logo: '',
@@ -17,18 +17,44 @@ const useTenantStore = defineStore('tenant', () => {
 
     const theme = useTheme()
 
+    setColorTheme(tenant.value.theme.colors)
+
+    function getFromStorage(key: string): any | null {
+        const item = JSON.parse(localStorage.getItem(key)!);
+
+        if (!item) return null;
+        
+        Object.entries(item ?? {}).forEach(([key, value]: any) => {
+            try {
+                if (typeof value === 'string') {
+                    item[key] = JSON.parse(value);
+                }
+            } catch (error) {
+                item[key] = value;
+            }
+        })
+
+        return item
+    }
+
+    function setColorTheme(injectColors: any) {
+        const colors = Object.assign({}, theme.current.value.colors, injectColors)
+
+        Object.assign(theme.themes.value.light, { colors })
+    }
+
     const loadTenant = async () => {
         try {
             return getTenant()
                 .then((resp) => {
+                    localStorage.setItem('tenant', JSON.stringify(resp.data));
+
                     tenant.value = {
                         ...resp.data,
                         theme: JSON.parse(resp.data.theme),
                     }
 
-                    const colors = Object.assign({}, theme.current.value.colors, tenant.value.theme.colors)
-                    
-                    Object.assign(theme.themes.value.light, { colors })
+                    setColorTheme(tenant.value.theme.colors)
                 })
         } catch (error) {
             console.error({error});
