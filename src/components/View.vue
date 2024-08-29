3586a8e5ea852926a31b59bb47b9d3deb4986edf
jonathas Augusto <jonathas_a.a@hotmail.com>
2024-08-29 15:32:56 -0300

    Adiciona portabilidade para trativa de mobile

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,46 +1,55 @@
 <template>
   <v-sheet class="pa-4" rounded color="on-surface">
     <v-row no-gutters>
-      <v-col cols="12">
+      <v-col cols="auto" class="mr-4">
         <div class="d-flex">
           <slot name="title-prepend"></slot>
           <h1 class="d-flex justify-center">
-            <v-icon v-if="icon" :icon="props.icon" class="mr-2">
-            </v-icon>
+            <v-icon v-if="icon" :icon="props.icon" class="mr-2"> </v-icon>
             <slot name="title">
               {{ title }}
             </slot>
           </h1>
           <slot name="title-append"></slot>
-          <div class="ml-auto d-flex align-center ga-4">
-            <v-btn v-if="btnActionText && (btnActionAcl ? authorization.acl(btnActionAcl): true)" :text="btnActionText" @click="emit('click:btnAction')" color="primary"></v-btn>
-            <slot name="action">
-                <slot name="action-prepend"></slot>
-
-                  <v-menu v-if="enableAction">
-                    <template #activator="{ props }">
-                      <v-btn
-                        v-if="authorization.acl(slugs)"
-                        :icon="actionsIcon"
-                        v-bind="props"
-                      ></v-btn>
-                    </template>
-                    <v-list>
-                    <template v-for="action in actions">
-                      <v-list-item
-                        :to="action.to"
-                        @click="action.click"
-                        v-if="authorization.acl(action.slug)"
-                      >
-                        <v-list-item-title>{{ action.title }}</v-list-item-title>
-                      </v-list-item>
-                    </template>
-                  </v-list>
-                </v-menu>
-              </slot>
-          </div>
         </div>
-        <p>
+      </v-col>
+      <v-col cols="auto" class="ml-auto d-flex align-center">
+        <v-btn
+          v-if="
+            btnActionText &&
+            (btnActionAcl ? authorization.acl(btnActionAcl) : true)
+          "
+          :text="btnActionText"
+          @click="emit('click:btnAction')"
+          color="primary"
+        ></v-btn>
+        <slot name="action">
+          <slot name="action-prepend"></slot>
+
+          <v-menu v-if="enableAction">
+            <template #activator="{ props }">
+              <v-btn
+                v-if="authorization.acl(slugs)"
+                :icon="actionsIcon"
+                v-bind="props"
+              ></v-btn>
+            </template>
+            <v-list>
+              <template v-for="action in actions">
+                <v-list-item
+                  :to="action.to"
+                  @click="action.click"
+                  v-if="authorization.acl(action.slug)"
+                >
+                  <v-list-item-title>{{ action.title }}</v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-list>
+          </v-menu>
+        </slot>
+      </v-col>
+      <v-col cols="12">
+        <p :class="{'mt-4': description}">
           <slot name="description">
             {{ description }}
           </slot>
@@ -53,11 +62,11 @@
   </v-sheet>
 </template>
 <script lang="ts" setup>
-import authorization from '@/plugins/authorization';
-import { computed } from 'vue';
+import authorization from "@/plugins/authorization";
+import { computed } from "vue";
 
 interface Emit {
-  (event: 'click:btnAction'): void;
+  (event: "click:btnAction"): void;
 }
 
 interface Action {
@@ -81,13 +90,13 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
   enableAction: false,
   actions: () => [],
-  btnActionText: '',
-  btnActionAcl: '',
+  btnActionText: "",
+  btnActionAcl: "",
   actionsIcon: "mdi-cog",
 });
 
 const emit = defineEmits<Emit>();
 const slugs = computed(() => {
-  return props.actions.map(action => action.slug).join('|')
-})
+  return props.actions.map((action) => action.slug).join("|");
+});
 </script>
