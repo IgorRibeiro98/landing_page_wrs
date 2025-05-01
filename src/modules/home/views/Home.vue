6f876729b9321dea11774738746752e6b3c49c3c
Jonathas Apostulo <jonathas_a.a@hotmail.com>
2025-05-01 15:11:21 -0300

    aleluia

--- src/modules/home/views/Home.vue
+++ src/modules/home/views/Home.vue
@@ -60,10 +60,33 @@
           em
           algo mais sério. Ou talvez você tenha um projeto criativo para compartilhar com o mundo.
         </p>
-        <v-btn class="mt-4">Entre em contato</v-btn>
+        <v-btn rounded class="mt-4">Entre em contato</v-btn>
       </div>
     </v-col>
   </v-row>
+  <div class="bg-white">
+    <v-container>
+      <v-row justify="space-between" class="pa-4">
+        <v-col cols="12" class="text-center">
+          <h2 class="text-h2 py-4">
+            Construindo casas novas <br> desde 1985
+          </h2>
+        </v-col>
+        <v-col cols="12" md="5">
+          <v-img src="https://picsum.photos/800/300?random=1" height="400" :width="mobile ? '100%': '70%'" cover class="mb-10"></v-img>
+          <p>
+            Tudo começa com uma ideia. Talvez você queira abrir um negócio. Talvez você queira transformar um passatempo em algo mais sério. Ou talvez você tenha um projeto criativo para divulgar ao mundo. Seja o que for, o modo de contar sua história on-line faz toda a diferença.
+          </p>
+          <p>
+            Não se preocupe em parecer profissional. Seja você mesmo. Existem mais de 1,5 bilhão de sites, mas a sua história é o diferencial que separa você dos outros. Se você ler as palavras em voz alta e não se sentir identificado, isso significa que o texto ainda precisa ser trabalhado.
+          </p>
+        </v-col>
+        <v-col cols="12" md="5">
+          <v-img src="https://picsum.photos/800/300?random=1" height="600" width="100%" cover></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, computed } from "vue";
@@ -95,3 +118,6 @@ const infoData = ref<InfoData[]>([
 
 ]);
 </script>
+<style>
+
+</style>
