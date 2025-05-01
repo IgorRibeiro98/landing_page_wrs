75243fb81e430225f8a32bd5467d5b47f32fdf56
Igor <igor.ribeiro@uotz.com.br>
2025-05-01 14:47:54 -0300

    Changes
    Changes to be committed:

--- src/modules/home/views/Home.vue
+++ src/modules/home/views/Home.vue
@@ -1,35 +1,38 @@
 <template>
-  <v-row justify="center" class="pa-4">
-    <v-col cols="12" md="10" class="text-right text-right">
-      <h1 class="font-weight-bold text-h1" style="line-height: 80%;">
-        Construtores <br> de casas novas
-      </h1>
-    </v-col>
-    <v-col cols="12">
-      <v-carousel hide-delimiters class="pt-5" height="100%" style="overflow: hidden;max-width: 100%">
-        <v-carousel-item v-for="(item, i) in 5" :key="i" class="text-left">
-          <v-img :src="`https://picsum.photos/800/300?random=${i}`" cover :height="mobile ? '200' : '500'"
-            width="100%"></v-img>
-        </v-carousel-item>
-        <template #prev="{ props }">
-          <v-btn icon color="white" v-bind="props">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </template>
-        <template #next="{ props }">
-          <v-btn icon color="white" v-bind="props">
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-btn>
-        </template>
-      </v-carousel>
-    </v-col>
-  </v-row>
+  <div :class="{ 'h-100': !mobile }">
+    <v-row justify="center" class="pa-4">
+      <v-col cols="12" class="text-right">
+        <h1 class="font-weight-bold text-h1 mb-15" style="line-height: 80%;">
+          Construtores <br> de casas novas
+        </h1>
+      </v-col>
+      <v-col cols="12" class="d-flex flex-column flex-grow-1">
+        <v-carousel hide-delimiters height="100%" style="overflow: hidden;max-width: 100%">
+          <v-carousel-item v-for="(item, i) in 5" :key="i" class="text-left">
+            <v-img :src="`https://picsum.photos/800/300?random=${i}`" cover :height="mobile ? '200' : '500'"
+              width="100%"></v-img>
+          </v-carousel-item>
+          <template #prev="{ props }">
+            <v-btn icon color="white" v-bind="props">
+              <v-icon>mdi-chevron-left</v-icon>
+            </v-btn>
+          </template>
+          <template #next="{ props }">
+            <v-btn icon color="white" v-bind="props">
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+          </template>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </div>
   <div class="bg-secondary">
     <v-row class="pa-4">
-      <v-col>
+      <v-col cols="12">
         <h2 class="text-h2">
           Nossos <br> serviços
         </h2>
+
       </v-col>
     </v-row>
   </div>
